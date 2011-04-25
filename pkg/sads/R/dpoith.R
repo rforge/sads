aebe3dc99844b023a315e5ffa9abf69f15aa1a09
cstrieder <cstrieder@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-25 04:53:58 +0000

    Rnw updated, modification related to dpoith.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/dpoith.R
+++ pkg/sads/R/dpoith.R
@@ -1,6 +1,6 @@
 require("zipfR")
-dpoith <- function(y, frac, m=0.1, M=50000) {
+dpoith <- function(y, frac, m=1, M=50) {
   b <- Igamma(y,frac*m,lower=FALSE) - Igamma(y,frac*M,lower=FALSE)
-  c <- factorial(y)*log(M/m)
+  c <- (factorial(y))*log(M/m)
   b/c
 }
