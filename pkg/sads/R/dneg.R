e31956612a408858577fc0bcae0b159441cf5930
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-25 01:03:28 +0000

    binomial coefficient in dneg changed to allow real-valued k, including 0>k<1 minor changes in identation of dnex
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/dneg.R
+++ pkg/sads/R/dneg.R
@@ -1,17 +1,16 @@
 dneg <- function(y, frac, rate, k=2, beta=1) {
-    s <- length(combinations(k+y-1,k-1)[,1])
-    b <- pi*s*(1/sin(pi*(beta-k)))
-    c <- gamma(beta)*gamma(k+y)
-	  
-    t1 <- (k*rate/frac)^k
-    t2 <- gamma(k+y)
-    t3 <- kummerM(k+y,1-beta+k,k*rate/frac)
-    d1 <- t1*t2*t3
-    
-    t1 <- (k*rate/frac)^k
-    t2 <- gamma(beta+y)
-    t3 <- kummerM(beta+y,1+beta-k,k*rate/frac)
-    d2 <- t1*t2*t3
-    
-    b/c*(d1-d2)
+  s <- exp(lgamma(k+y)-(lgamma(k)+lgamma(y)))
+  b <- pi*s*(1/sin(pi*(beta-k)))
+  c <- gamma(beta)*gamma(k+y)
+
+  t1 <- (k*rate/frac)^k
+  t2 <- gamma(k+y)
+  t3 <- kummerM(k+y,1-beta+k,k*rate/frac)
+  d1 <- t1*t2*t3
+  
+  t4 <- gamma(beta+y)
+  t5 <- kummerM(beta+y,1+beta-k,k*rate/frac)
+  d2 <- t1*t4*t5
+  
+  b/c*(d1-d2)
 }
