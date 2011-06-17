f220f999848ff0f63a1d0a5f16188a8b05ae1847
cstrieder <cstrieder@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-17 06:11:28 +0000

    integer check added to first arg of dpoix and dpoig
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/dpoig.R
+++ pkg/sads/R/dpoig.R
@@ -1,5 +1,12 @@
 dpoig <- function(y, frac, rate, shape) {
-  b <- y*log(frac)+shape*log(rate)+lgamma(y+shape)
-  c <- lfactorial(y)+lgamma(shape)+(y+shape)*log(frac+rate)
-  exp(b-c)
+    is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)
+        abs(x - round(x)) < tol
+    if(FALSE %in% sapply(y,is.wholenumber))
+        print("y must be integer because dpoig is a discrete PDF.")
+    else {
+        b <- y*log(frac)+shape*log(rate)+lgamma(y+shape)
+        c <- lfactorial(y)+lgamma(shape)+(y+shape)*log(frac+rate)
+        exp(b-c)
+    }
 }
+
--- pkg/sads/R/dpoix.R
+++ pkg/sads/R/dpoix.R
@@ -1,6 +1,12 @@
-dpoix <- function(y, frac=0.05, rate=1/1000, log=FALSE) {  
-	  b <- y*log(frac)
-	  m <- log(rate)
-	  n <- (y+1)*log(rate+frac)
-    if(log)b+m-n else exp(b+m-n)
+dpoix <- function(y, frac, rate, log=FALSE) {
+    is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)
+        abs(x - round(x)) < tol
+    if(FALSE %in% sapply(y,is.wholenumber))
+       print("y must be integer because dpoix is a discrete PDF.")
+    else {
+        b <- y*log(frac)
+	      m <- log(rate)
+	      n <- (y+1)*log(rate+frac)
+        if(log)b+m-n else exp(b+m-n)
+    }
 }
