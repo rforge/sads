19bcb223035fd5996c3996cb171869b2b4514535
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-29 18:54:18 +0000

    Man pages with wrong informations deleted Minor changes in some density functions (argument values, truncation and log included, etc)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/dneg.R
+++ pkg/sads/R/dneg.R
@@ -1,15 +1,15 @@
-dneg <- function(y, frac, rate, k=2, beta=1) {
+dneg <- function(y, frac, rate, shape, k) {
   s <- exp(lgamma(k+y)-(lgamma(k)+lgamma(y)))
-  b <- pi*s*(1/sin(pi*(beta-k)))
-  c <- gamma(beta)*gamma(k+y)
+  b <- pi*s*(1/sin(pi*(shape-k)))
+  c <- gamma(shape)*gamma(k+y)
 
   t1 <- (k*rate/frac)^k
   t2 <- gamma(k+y)
-  t3 <- kummerM(k+y,1-beta+k,k*rate/frac)
+  t3 <- kummerM(k+y,1-shape+k,k*rate/frac)
   d1 <- t1*t2*t3
   
-  t4 <- gamma(beta+y)
-  t5 <- kummerM(beta+y,1+beta-k,k*rate/frac)
+  t4 <- gamma(shape+y)
+  t5 <- kummerM(shape+y,1+shape-k,k*rate/frac)
   d2 <- t1*t4*t5
   
   b/c*(d1-d2)
--- pkg/sads/R/dpoig.R
+++ pkg/sads/R/dpoig.R
@@ -1,14 +1,16 @@
-dpoig <- function(y, frac, rate, shape, trunc=0) {
+dpoig <- function(y, frac, rate, shape, trunc=NULL, log=FALSE) {
     is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)
         abs(x - round(x)) < tol
     if(FALSE %in% sapply(y,is.wholenumber))
-        print("y must be integer because dpoig is a discrete PDF.")
+        stop("y must be integer because dpoig is a discrete PDF.")
     else {
         f <- function(y) {
             b <- y*log(frac)+shape*log(rate)+lgamma(y+shape)
             c <- lfactorial(y)+lgamma(shape)+(y+shape)*log(frac+rate)
             exp(b-c)
-        }
-        f(y)/(1-f(trunc))
-    }
+          }
+        if(is.null(trunc)) vals <- f(y)
+        else vals <- f(y)/(1-f(trunc))
+        if(log)log(vals) else vals
+      }
 }
