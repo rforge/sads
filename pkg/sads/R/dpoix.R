db0064eeaf9478e2d068ea7f1718e10ce66b8b22
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-05 07:19:51 +0000

    Corrections in dpoix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/dpoix.R
+++ pkg/sads/R/dpoix.R
@@ -1,15 +1,18 @@
 dpoix <- function(y, frac, rate, trunc=0, log=FALSE) {
-    is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)
-        abs(x - round(x)) < tol
-    if(FALSE %in% sapply(y,is.wholenumber))
-        print("y must be integer because dpoix is a discrete PDF.")
-    else {
-        f <- function(y){
-            b <- y*log(frac)
-            m <- log(rate)
-	    n <- (y+1)*log(rate+frac)
-            if(log)b+m-n else exp(b+m-n)
-        }
-        f(y)/(1-f(trunc))
+    is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
+      abs(x - round(x)) < tol
     }
-}
+    if(sum(y,is.wholenumber(y))<length(y))
+       stop("dpoix is a discrete PDF; all y's must be integers")
+    else {
+      f <- function(y){
+        b <- y*log(frac)
+        m <- log(rate)
+        n <- (y+1)*log(rate+frac)
+        if(log) b+m-n else exp(b+m-n)
+      }
+      if(!is.null(trunc)) f(y)/(1-sum(f(0:trunc))) else f(y)
+    }
+  }
+       
+    
