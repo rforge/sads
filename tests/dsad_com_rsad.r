a3aae6a88ea4c6354d7f2a2eb81c2de4cacddb55
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-16 03:57:13 +0000

    Failed try to set dsad based on simulations with rsad
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/dsad_com_rsad.r
+++ tests/dsad_com_rsad.r
@@ -25,3 +25,29 @@ Fn2(28)
 Fn2(29)
 Fn2(30)
 Fn2(29)==Fn2(30)
+
+## Um teste qquick and dirty
+dsadr <- function(x,N=1e5,...){
+  y <- rsad(N,...)
+  z <- length(y)/N
+  new.N <- (N-length(y))/z
+  y <- c(y,rsad(new.N,...))
+  ty <- table(y)
+  Fn <- approxfun(x=as.numeric(names(ty)), y=ty/sum(ty), method="constant", f=1, rule=2)
+  py <- Fn(x)
+  py[py==0] <- 1/sum(ty) # gambiarra para nao dar probabilidades =0
+}
+## Teste
+y <- dsadr(1:20,N=1e6,frac=0.1,sad=lnorm,samp="Poisson",meanlog=3,sdlog=1.5)
+x <- dpoilog(1:20,mu=3+log(0.1),sig=1.5)
+plot(x,y, log="xy"); abline(0,1) ## mto bom
+## Teste de ajuste
+dados <- rsad(100,frac=0.1,sad=lnorm,samp="Poisson",meanlog=3,sdlog=2)
+LL <- function(m,s){
+  -sum(log(dsadr(dados,frac=0.1,sad=lnorm,samp="Poisson",meanlog=m,sdlog=s)))
+}
+teste <- mle2(LL,start=list(m=1,s=1))
+teste2 <- poilogMLE(dados)
+coef(teste)
+teste2$par
+## Nao funfou: demora, dá erros e estimativas muito fora
