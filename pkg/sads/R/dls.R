40cd1ca082307d5ab0afc3996654923d5c8a4f8d
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-29 01:13:06 +0000

    ptrunc changed to allow for arguments lower.tail=FALSE and log.p=TRUE, must be checked! Minor changes in dls.R --Esta linha, e as abaixo, serão ignoradas--
    M    pkg/sads/R/dls.R
    M    pkg/sads/R/ptrunc.R
    M    tests/.Rhistory
    M    tests/.RData
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/dls.R
+++ pkg/sads/R/dls.R
@@ -3,7 +3,7 @@ dls <- function(x, N, alpha, log = FALSE){
   if (N <= 0)  stop("N must be larger than zero")
   if (alpha <= 0)  stop("alpha must be larger than zero")
   if(any(x < 1)) stop("at least one x less than one")
-  if(any(!is.wholenumber(x))) stop("at least one non-integer x or all x must be integers")
+  if(any(!is.wholenumber(x))) stop("all x must be integers")
   X <- N/(N+alpha)
   gama <- function(y) {1/log(1/(1-y))}
   y <- gama(X)*(X^x)/x
--- pkg/sads/R/ptrunc.R
+++ pkg/sads/R/ptrunc.R
@@ -1,14 +1,32 @@
 ptrunc <- function(f, q, trunc, ...){
+  log.p <- FALSE
+  lower.tail <- FALSE
   dots <- c(list(...))
+  if("log.p"%in%names(dots)){
+    log.p <- dots$log.p
+    dots$log.p <- FALSE
+  }
+  if("log"%in%names(dots)){
+    log.p <- dots$log
+    dots$log <- FALSE
+  }
+  dots2 <- dots
+  if("lower.tail"%in%names(dots2)){
+    lower.tail <- dots2$lower.tail
+    dots2$lower.tail <- FALSE
+  }
   tt <- q
   pf <- get(paste("p", f, sep = ""), mode = "function")
   if(!missing(trunc)){
     aa <- rep(trunc, length(q))
     tt <- do.call(pf, c(list(q = apply(cbind(q, aa), 1, max)), dots))
-    tt <- tt - do.call(pf, c(list(q = aa), dots))
-    tt <- tt/(1 - do.call(pf, c(list(q = aa), dots)))
-  } else{
+    if(lower.tail){
+      tt <- tt - do.call(pf, c(list(q = aa), dots))
+    }
+    tt <- tt/(1 - do.call(pf, c(list(q = aa), dots2)))
+  }
+  else{
     tt <- do.call(pf, c(list(q = q), dots))
   }
-  return(tt)
+  if(log.p)return(log(tt)) else return(tt)
 }
