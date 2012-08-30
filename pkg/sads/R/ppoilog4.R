2cb17026d64500fbb039fa12860ba2603cc7b038
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-30 21:42:36 +0000

    Old versions of d and q poilog function removed Negative-binomial truncated hyperbolic removed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/ppoilog4.R
+++
@@ -1,9 +0,0 @@
-ppoilog4 <- function(q, mu, sig, lower.tail=TRUE, log.p=FALSE){
-  if (length(mu) > 1 | length(sig) > 1) stop("vectorization of mu and sig is currently not implemented")
-  if (!all(is.finite(c(mu, sig)))) stop("all parameters should be finite")
-  if (sig <= 0) stop("sig is not larger than zero")
-  y <- sapply(q, function(n) sum(poilog::dpoilog(0:n, mu, sig)))
-  if(!lower.tail) y <- 1-y
-  if(log.p) y <- log(y)
-  return(y)
-}
