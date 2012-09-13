388e28803c470bb7095ab5382954489f353ef01e
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-13 19:03:28 +0000

    "sad" argument in rsad changed to character to make compatible to the other functions; help changed accordingly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@145 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/rsad.R
+++ pkg/sads/R/rsad.R
@@ -1,10 +1,10 @@
-rsad <- function(S,frac,sad, samp=c("Poisson","Negbinom"),k,zeroes=FALSE,...){
-  sad <- paste("r",deparse(substitute(sad)),sep="")
+rsad <- function(S,frac,sad, Pois.samp=TRUE,k,zeroes=FALSE,...){
+  sad <- paste("r",sad,sep="")
   dots <- list(...)
   com <- do.call(sad,c(list(n=S),dots))
-  sam <- switch(samp,
-                Poisson=rpois(S,lambda=frac*com),
-                Negbinom=rnbinom(S,mu=frac*com,size=k)
-                )
+  if(Pois.samp) sam=rpois(S,lambda=frac*com)
+  else {
+      if(missing(k))stop("For negative binomial sampling please provide a value for k")
+      else sam=rnbinom(S,mu=frac*com,size=k)}
   if(zeroes)sam else sam[sam>0]
 }
