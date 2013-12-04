2524ef9fd9547de44eb076a6c1f56ca4f3194bba
piklprado <piklprado@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-04 03:05:36 +0000

    corrections in finbinom (explicit arguments size and mu in LL functions) and some tasks added to todolist
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sads@179 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sads/R/fitnbinom.R
+++ pkg/sads/R/fitnbinom.R
@@ -1,4 +1,4 @@
-fitnbinom <- function(x, trunc, start.value, ...){
+fitnbinom <- function(x, trunc=0, start.value, ...){
   dots <- list(...)
   if (!missing(trunc)){
     if (min(x)<=trunc) stop("truncation point should be lower than the lowest data value")
@@ -6,14 +6,15 @@ fitnbinom <- function(x, trunc, start.value, ...){
   if(missing(start.value)){ #Olhar os chutes default
     muhat <- length(x)/(length(x) + mean(x))
     sizehat <- muhat*mean(x) 
-  } else{
+  }
+  else{
     sizehat <- start.value[[1]]
     muhat <- start.value[[2]]
   }
   if (missing(trunc)){
-    LL <- function(size, mu) -sum(dnbinom(x, size, mu, log = TRUE))
+    LL <- function(size, mu) -sum(dnbinom(x, size=size, mu=mu, log = TRUE))
   } else{
-    LL <- function(size, mu) -sum(dtrunc("nbinom", x = x, coef = list(size, mu), trunc = trunc, log = TRUE))
+    LL <- function(size, mu) -sum(dtrunc("nbinom", x = x, coef = list(size=size, mu=mu), trunc = trunc, log = TRUE))
   }
   result <- mle2(LL, start = list(size = sizehat, mu = muhat), data = list(x = x), ...)
   new("fitsad", result, sad="nbinom", distr = "D", trunc = ifelse(missing(trunc), NaN, trunc))
